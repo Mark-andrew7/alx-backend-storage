c895c0bf3d6062e02e5c9ed3e630dcdac3c21cf7
Mark-andrew7 <kamaumark19@gmail.com>
2024-07-16 20:34:13 +0300

    Removed trailing white space

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,10 @@
-""" 
+"""
 Python function that lists all documents in a collection
 """
 
 
 def list_all(mongo_collection):
-    """ 
+    """
     Function that lists all documents in a collection
     """
     if not mongo_collection:
