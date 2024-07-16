3a2ac8685679f7b7d4c15003ee19d7af14c56393
Mark-andrew7 <kamaumark19@gmail.com>
2024-07-16 20:28:20 +0300

    List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+""" 
+Python function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """ 
+    Function that lists all documents in a collection
+    """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
